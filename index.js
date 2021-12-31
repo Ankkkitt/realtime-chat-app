78041b4510a2948e2e58ce225d6aa6f7fe6e38dd
Ankit Upadhyay <72919994+Ankkkitt@users.noreply.github.com>
2021-12-31 13:30:17 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+// making  socket io connnection 
+const io = require('socket.io')(http);
+
+io.sockets.on('connection', function(socket){
+    console.log('user connected ...');
+
+    // broadcasting the message that come from browser
+    socket.on('send-chat-message', (data) => {
+        io.emit('chat-message', (data));        //  io. emit() to send a message to all the connected clients
+        console.log(data);
+    })
+
+    socket.on('disconnect', () =>{
+        console.log('user disconnected ...')
+    })
+})
+
+http.listen(3000, ()=>{
+    console.log(`Server is running on port 3000`);
+});
